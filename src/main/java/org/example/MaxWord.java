b9f0d7358466d26ee1d69786b6fed6fcbaba861e
codivers <career@codivers.com>
2023-05-28 19:14:45 -0400

    do assignment for max word.

---
+++ src/main/java/org/example/MaxWord.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class MaxWord {
+
+
+    public static void main(String[] args) {
+        String[] sentan =  {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+    }
+
+    public int maxWord(String[] arr) {
+
+
+
+        return 0;
+    }
+}
