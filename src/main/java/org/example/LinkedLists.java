ef2f019e280050d3b39a839e679b1d8a10a0e062
proethiopia <mintesnotbissare@gmail.com>
2023-08-01 17:45:00 -0400

    deletenode

--- src/main/java/org/example/LinkedLists.java
+++ src/main/java/org/example/LinkedLists.java
@@ -6,74 +6,78 @@ import java.util.LinkedList;
 
 public class LinkedLists {
 
+  Node head;
+  private static class Node {
+    int item;
+    Node next;
+    Node prev;
+    Node(int val) { item = val; }
+  }
 
-    Node head;
-    private static class Node {
-        int item;
-        Node next;
-        Node prev;
-        Node(int val) {
-            item = val;
-        }
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<>();
+    cars.add("Toyota");
+    cars.add("Ford");
+    cars.add("Honda");
+
+    LinkedLists list = new LinkedLists();
+
+    list = insertInto(list, 5);
+    list = insertInto(list, 2);
+    list = insertInto(list, 1);
+    list = insertInto(list, 7);
+
+    printList(list);
+  }
+
+  public static LinkedLists insertInto(LinkedLists list, int data) {
+
+    Node newNode = new Node(data);
+    if (list.head == null) {
+      list.head = newNode;
+    } else {
+      Node last = list.head;
+
+      while (last.next != null) {
+        last = last.next;
+      }
+
+      last.next = newNode;
     }
 
+    return list;
+  }
 
+  public static void printList(LinkedLists list) {
+    Node currNode = list.head;
 
+    System.out.print("LinkedList: ");
 
-    public static void main(String[] args) {
-            LinkedList<String> cars = new LinkedList<>();
-            cars.add("Toyota");
-            cars.add("Ford");
-            cars.add("Honda");
+    while (currNode != null) {
+      System.out.print(currNode.item + " ");
 
-
-
-        LinkedLists list = new LinkedLists();
-
-
-        list = insertInto(list, 5);
-        list = insertInto(list, 2);
-        list = insertInto(list, 1);
-        list = insertInto(list, 7);
-
-
-        printList(list);
-
-
+      // Go to next node
+      currNode = currNode.next;
     }
+  }
+  public static void deleteNode(LinkedLists list, int value) {
+    Node currNode = list.head;
+    Node prevNode = null;
 
-
-    public static LinkedLists insertInto(LinkedLists list, int data) {
-
-        Node newNode = new Node(data);
-        if (list.head == null) {
-            list.head = newNode;
+    while (currNode != null) {
+      if (currNode.item == value) {
+        if (prevNode == null) {
+          // If the node to be deleted is the head node
+          list.head = currNode.next;
         } else {
-            Node last = list.head;
-
-            while (last.next != null) {
-                last = last.next;
-            }
-
-            last.next = newNode;
-
+          // If the node to be deleted is not the head node
+          prevNode.next = currNode.next;
         }
-
-        return list;
-
+      } else {
+        // Move to the next node
+        prevNode = currNode;
+      }
+      currNode = currNode.next;
     }
-
-    public static void printList(LinkedLists list)
-    {
-        Node currNode = list.head;
-
-        System.out.print("LinkedList: ");
-
-        while (currNode != null) {
-            System.out.print(currNode.item + " ");
-
-            // Go to next node
-            currNode = currNode.next;
-        }
-    }
+  }
 }
